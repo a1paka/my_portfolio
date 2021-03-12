26c14120d07c8e86bf0cf7b71813f791bcb90b03
Anna <anya_aki@mail.ru>
2021-03-12 22:45:29 +0300

    переделка

--- source/js/main.js
+++ source/js/main.js
@@ -1,17 +1,34 @@
-let navMain = document.querySelector(".main-nav");
-let navToggle = document.querySelector(".main-nav__toggle");
+// let navMain = document.querySelector(".main-nav");
+// let navToggle = document.querySelector(".main-nav__toggle");
 
-navMain.classList.remove("main-nav--nojs");
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-  } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
-  }
+// navMain.classList.remove("main-nav--nojs");
+// navToggle.addEventListener('click', function() {
+//   if (navMain.classList.contains("main-nav--closed")) {
+//     navMain.classList.remove("main-nav--closed");
+//     navMain.classList.add("main-nav--opened");
+//   } else {
+//     navMain.classList.add("main-nav--closed");
+//     navMain.classList.remove("main-nav--opened");
+//   }
+// });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.main-nav'),
+  menuItem = document.querySelectorAll('.main-nav__item'),
+  hamburger = document.querySelector('.main-header__hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('main-header__hamburger-active');
+      menu.classList.toggle('main-nav__active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('main-header__hamburger-active');
+          menu.classList.toggle('main-nav__active');
+      })
+  })
 });
-
 
 $('.slider').slick({
   arrows: true,
